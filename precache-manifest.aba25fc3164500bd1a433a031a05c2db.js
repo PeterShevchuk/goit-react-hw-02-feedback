0e1a84a0d0ffa4fad28a95f61e7907f00dc948d6
Peter Shevchuk <raf02041994@gmail.com>
2020-08-17 15:16:48 +0300

    Updates

---
+++ precache-manifest.aba25fc3164500bd1a433a031a05c2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d318864702ca704804e13180a107b4d4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c1a739fc7e00024f967c",
+    "url": "/goit-react-hw-02-feedback/static/css/main.e9e6bb6c.chunk.css"
+  },
+  {
+    "revision": "99714676910226e44d2e",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bb9cca06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bb9cca06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a739fc7e00024f967c",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e9528de1.chunk.js"
+  },
+  {
+    "revision": "509450bfce09ae6ef313",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.fcc87033.js"
+  }
+]);
